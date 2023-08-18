935414c2b1254ddc5e7ec89f3c65eadf5d13e455
Igor Mariano <igor-mariano@outlook.com>
2023-08-18 14:42:44 -0300

    Criando pasta com os arquivos do projeto Ikaros usando spring boot

---
+++ projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/ProjetoikarosApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetoikaros.projetoikaros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoikarosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoikarosApplication.class, args);
+	}
+
+}
---
+++ projetoikaros/src/test/java/br/com/projetoikaros/projetoikaros/ProjetoikarosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoikaros.projetoikaros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoikarosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
