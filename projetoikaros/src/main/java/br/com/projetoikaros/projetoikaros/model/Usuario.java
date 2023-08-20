7235b043e8322e4c93293c39e280a7bf2e32b818
Igor Mariano <igor-mariano@outlook.com>
2023-08-20 13:59:41 -0300

    Criando o atributo id e gerando getters e setters

--- projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/model/Usuario.java
+++ projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/model/Usuario.java
@@ -1,5 +1,13 @@
 package br.com.projetoikaros.projetoikaros.model;
 
 public class Usuario {
-    
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
