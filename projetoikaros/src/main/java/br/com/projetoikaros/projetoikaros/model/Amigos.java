b936c2c6a9cf38d2ce455c4572a7322c56725925
Igor Mariano <igor-mariano@outlook.com>
2023-08-20 16:27:29 -0300

    Criando atributos para a classe Amigos + getters e setters para os métodos

--- projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/model/Amigos.java
+++ projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/model/Amigos.java
@@ -1,5 +1,31 @@
 package br.com.projetoikaros.projetoikaros.model;
 
 public class Amigos {
-    
+    private Integer amizadeId;
+    private Usuario relacionamentoAmizade1;
+    private Usuario relacionamentoAmizade2;
+
+    public Integer getAmizadeId() {
+        return this.amizadeId;
+    }
+
+    public void setAmizadeId(Integer amizadeId) {
+        this.amizadeId = amizadeId;
+    }
+
+    public Usuario getRelacionamentoAmizade1() {
+        return this.relacionamentoAmizade1;
+    }
+
+    public void setRelacionamentoAmizade1(Usuario relacionamentoAmizade1) {
+        this.relacionamentoAmizade1 = relacionamentoAmizade1;
+    }
+
+    public Usuario getRelacionamentoAmizade2() {
+        return this.relacionamentoAmizade2;
+    }
+
+    public void setRelacionamentoAmizade2(Usuario relacionamentoAmizade2) {
+        this.relacionamentoAmizade2 = relacionamentoAmizade2;
+    }
 }
