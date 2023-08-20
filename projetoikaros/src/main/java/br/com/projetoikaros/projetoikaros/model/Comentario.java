12dff846bf5a699899acce6843cceb0897593b64
Igor Mariano <igor-mariano@outlook.com>
2023-08-20 15:35:03 -0300

    Criando o atributo data_publicacao_comentario e gerando getters e setters referente ao novo atributo

--- projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/model/Comentario.java
+++ projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/model/Comentario.java
@@ -1,7 +1,10 @@
 package br.com.projetoikaros.projetoikaros.model;
 
+import java.time.LocalDate;
+
 public class Comentario {
     private Integer id;
+    private LocalDate data_publicacao_comentario;
 
     public Integer getId() {
         return id;
@@ -9,5 +12,13 @@ public class Comentario {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public LocalDate getData_publicacao_comentario() {
+        return data_publicacao_comentario;
+    }
+
+    public void setData_publicacao_comentario(LocalDate data_publicacao_comentario) {
+        this.data_publicacao_comentario = data_publicacao_comentario;
     }
 }
