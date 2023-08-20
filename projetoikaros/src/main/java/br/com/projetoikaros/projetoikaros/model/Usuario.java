14165f46e1e5c2d7a0280ce7571da84b77ef8766
Igor Mariano <igor-mariano@outlook.com>
2023-08-20 14:02:36 -0300

    Criando o atributo sobrenome e gerando getters e setters referente ao novo atributo

--- projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/model/Usuario.java
+++ projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/model/Usuario.java
@@ -3,20 +3,28 @@ package br.com.projetoikaros.projetoikaros.model;
 public class Usuario {
     private Integer id;
     private String nome;
+    private String sobrenome;
 
     public Integer getId() {
         return id;
     }
-
+    
     public void setId(Integer id) {
         this.id = id;
     }
-
+    
     public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+    public String getSobrenome() {
+        return this.sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
     }
 }
