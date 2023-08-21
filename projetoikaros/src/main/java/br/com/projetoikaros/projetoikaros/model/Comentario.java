6ea52138787eef837509a7010042ec4d1ed23989
Igor Mariano <igor-mariano@outlook.com>
2023-08-21 08:19:16 -0300

    Adicionando novos  métodos do tipo LocalDateTime

--- projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/model/Comentario.java
+++ projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/model/Comentario.java
@@ -1,7 +1,7 @@
 package br.com.projetoikaros.projetoikaros.model;
 
-import java.time.LocalDate;
-
+import java.time.Clock;
+import java.time.LocalDateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -23,7 +23,7 @@ public class Comentario {
     private Usuario usuarioQueComento;
 
     @Column(name = "Data_de_Publicacao", nullable = false)
-    private LocalDate data_publicacao_comentario;
+    private LocalDateTime data_publicacao_comentario;
 
     @Column(name = "conteudo_comentario", length = 100, nullable = false)
     private String conteudo;
@@ -39,12 +39,20 @@ public class Comentario {
         this.id = id;
     }
 
-    public LocalDate getData_publicacao_comentario() {
+    public LocalDateTime getData_publicacao_comentario() {
         return data_publicacao_comentario;
     }
 
-    public void setData_publicacao_comentario(int ano, int mes, int dia) {
+    public void setData_publicacao_comentario(LocalDateTime data_publicacao_comentario) {
         this.data_publicacao_comentario = data_publicacao_comentario;
+    }
+
+    public LocalDateTime setData_publicacao_comentario() {
+        return now(Clock.systemDefaultZone());
+    }
+
+    private LocalDateTime now(Clock clock) {
+        return LocalDateTime.now(clock);
     }
 
     public String getConteudo() {
