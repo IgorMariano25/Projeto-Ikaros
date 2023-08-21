0d74c7715c128f31d27351f11d814a34c40ac377
Igor Mariano <igor-mariano@outlook.com>
2023-08-21 10:14:32 -0300

    Adicionando @Data

--- projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/model/Usuario.java
+++ projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/model/Usuario.java
@@ -7,7 +7,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "Usuario")
 public class Usuario {
