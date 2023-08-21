e34ab058ac55acae01991433ffb5358acef2d0ce
DeveSerOGui <guifelixs@gmail.com>
2023-08-21 12:21:57 -0300

    Criação das Requisições de Postagem

--- projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/controller/PostagemController.java
+++ projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/controller/PostagemController.java
@@ -23,22 +23,90 @@ import br.com.projetoikaros.projetoikaros.model.Usuario;
 @RequestMapping("/postagem")
 public class PostagemController {
 
+    private static ArrayList<Postagem> Postagens = new ArrayList<>();
+
     @GetMapping
     public ResponseEntity<List<Postagem>> getAll() {
         try {
-            List<Postagem> items = new ArrayList<Postagem>();
-            Postagem postagem1 = new Postagem();
-            // postagem1.setId(1);
-            postagem1.setUsuarioPublicador(null);
-            postagem1.setConteudoPost("Texto do post 1");
-            postagem1.setImagem("Url/aaaaaaaa");
-            postagem1.setCurtidas(112);
-            postagem1.setHoraPublicacao();
-
-            items.add(postagem1);
-            return new ResponseEntity<>(items, HttpStatus.OK);
+            return new ResponseEntity<>(Postagens, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Postagem> getById(@PathVariable("id") Integer id) {
+
+        Postagem result = null;
+
+        for (Postagem item : Postagens) {
+            if (item.getId() == id) {
+                result = item;
+                break;
+            }
+        }
+
+        if (result != null) {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Postagem> create(@RequestBody Postagem item) {
+        try {
+            Postagens.add(item);
+            return new ResponseEntity<>(item, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Postagem> update(@PathVariable("id") Integer id, @RequestBody Postagem PostagemNovosDados) {
+        
+        Postagem PostagemASerAtualizado = null;
+
+        for (Postagem item : Postagens) {
+            if (item.getId() == id) {
+                PostagemASerAtualizado = item;
+                break;
+            }
+        }
+
+        if (PostagemASerAtualizado == null ) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        PostagemASerAtualizado.setConteudoPost(PostagemNovosDados.getConteudoPost());
+        PostagemASerAtualizado.setImagem(PostagemNovosDados.getImagem());
+
+        return new ResponseEntity<>(PostagemASerAtualizado, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> delete(@PathVariable("id") Integer id) {
+        try {
+
+            Postagem PostagemASerExcluido = null;
+
+            for (Postagem item : Postagens) {
+                if (item.getId() == id) {
+                    PostagemASerExcluido = item;
+                    break;
+                }
+            }
+
+            // Não achei a pessoa a ser excluida
+            if (PostagemASerExcluido == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+            Postagens.remove(PostagemASerExcluido);
+
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
         }
     }
     
