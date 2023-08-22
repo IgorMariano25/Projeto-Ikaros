b6d38eda61eeffba936cbf34e5a40ded6161b2f9
Igor Mariano <igor-mariano@outlook.com>
2023-08-22 15:01:33 -0300

    Criando o arquivo NotificacacoesRepository.java

---
+++ projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/repository/NotificacacoesRepository.java
@@ -0,0 +1,8 @@
+package br.com.projetoikaros.projetoikaros.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.projetoikaros.projetoikaros.model.Notificacoes;
+
+public interface NotificacacoesRepository extends JpaRepository<Notificacoes, Long>{
+    
+}
