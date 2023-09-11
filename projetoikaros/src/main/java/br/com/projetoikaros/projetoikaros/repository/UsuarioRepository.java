1e0b33913b843496249f04520f5c58b7bf04f571
Igor Mariano <igor-mariano@outlook.com>
2023-09-11 14:54:58 -0300

    Criando método countById

--- projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/repository/UsuarioRepository.java
+++ projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/repository/UsuarioRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.projetoikaros.projetoikaros.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-    
+    long countById(Long Id);
 }
