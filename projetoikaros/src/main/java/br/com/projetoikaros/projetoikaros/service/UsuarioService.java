392d7ba547627d281a109d691f533a48abda632d
Igor Mariano <igor-mariano@outlook.com>
2023-09-11 14:55:25 -0300

    Modificando UsuarioService

--- projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/service/UsuarioService.java
+++ projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/service/UsuarioService.java
@@ -11,6 +11,21 @@ import org.springframework.stereotype.Service;
 import br.com.projetoikaros.projetoikaros.model.Usuario;
 import br.com.projetoikaros.projetoikaros.repository.UsuarioRepository;
 
+@Service
 public class UsuarioService {
-    
+
+    @Autowired
+    private UsuarioRepository _usuarioRepository;
+
+    public List<Usuario> findAll() {
+        return this._usuarioRepository.findAll();
+    }
+
+    public Usuario save(Usuario usuario) throws Exception {
+        if (this._usuarioRepository.countById(usuario.getId()) > 0) {
+            throw new Exception("Este ID já existe na base de dados");
+        }
+        this._usuarioRepository.save(usuario);
+        return usuario;
+    }
 }
