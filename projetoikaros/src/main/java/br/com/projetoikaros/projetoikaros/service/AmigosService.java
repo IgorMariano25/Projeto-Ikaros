ee2958dbbb4bdfa768f92e62d1551284bf5d225f
Igor Mariano <igor-mariano@outlook.com>
2023-09-11 14:29:30 -0300

    Adicionando imports para AmigosService

--- projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/service/AmigosService.java
+++ projetoikaros/src/main/java/br/com/projetoikaros/projetoikaros/service/AmigosService.java
@@ -1,5 +1,16 @@
 package br.com.projetoikaros.projetoikaros.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import br.com.projetoikaros.projetoikaros.model.Amigos;
+import br.com.projetoikaros.projetoikaros.repository.AmigosRepository;
+
 public class AmigosService {
     
 }
