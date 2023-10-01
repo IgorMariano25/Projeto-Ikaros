9de88e3200542a8d01b55ff203b0b939464687f8
Igor Mariano <igor-mariano@outlook.com>
2023-10-01 16:29:20 -0300

    Criando métodos findAll e FindById de NotificacoesService

--- src/main/java/br/com/ibmec/projetocloud/ikaros/service/NotificacoesService.java
+++ src/main/java/br/com/ibmec/projetocloud/ikaros/service/NotificacoesService.java
@@ -9,8 +9,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.ibmec.projetocloud.ikaros.model.Notificacoes;
-import br.com.ibmec.projetocloud.ikaros.repository.NotificacacoesRepository;
+import br.com.ibmec.projetocloud.ikaros.repository.NotificacoesRepository;
 
 public class NotificacoesService {
-    
+    @Autowired
+    private NotificacoesRepository _notificacoesRepository;
+
+    public List<Notificacoes> findAll() {
+        return this._notificacoesRepository.findAll();
+    }
+
+    public Optional<Notificacoes> findById(Long id) {
+        return this._notificacoesRepository.findById(id);
+    }
 }
