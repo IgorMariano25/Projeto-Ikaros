7635307570f9432e02044d29f23e4318d5961c6f
Igor Mariano <igor-mariano@outlook.com>
2023-10-01 10:30:07 -0300

    Criando o arquivo ProjetoikarosApplication

---
+++ src/main/java/br/com/ibmec/projetocloud/ikaros/ProjetoikarosApplication.java
@@ -0,0 +1,13 @@
+package br.com.ibmec.projetocloud.ikaros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoikarosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoikarosApplication.class, args);
+	}
+
+}
