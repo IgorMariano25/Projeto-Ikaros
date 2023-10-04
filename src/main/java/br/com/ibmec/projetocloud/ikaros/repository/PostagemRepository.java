5c2ae3b9ce357b395916ce724d06e773fad7313e
Igor Mariano <igor-mariano@outlook.com>
2023-10-04 20:57:26 -0300

    Criando método findById para PostagemRepository

--- src/main/java/br/com/ibmec/projetocloud/ikaros/repository/PostagemRepository.java
+++ src/main/java/br/com/ibmec/projetocloud/ikaros/repository/PostagemRepository.java
@@ -1,8 +1,10 @@
 package br.com.ibmec.projetocloud.ikaros.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.ibmec.projetocloud.ikaros.model.Postagem;
 
-public interface PostagemRepository extends JpaRepository<Postagem, Long>{
-   
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+    Optional<Postagem> findById(Long Id);
 }
