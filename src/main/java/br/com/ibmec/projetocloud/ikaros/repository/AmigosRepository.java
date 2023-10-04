6be306a3b1f151c094b253e6deaa8f2f3f79515a
Igor Mariano <igor-mariano@outlook.com>
2023-10-04 20:52:35 -0300

    Criando método findById para AmigosRepository

--- src/main/java/br/com/ibmec/projetocloud/ikaros/repository/AmigosRepository.java
+++ src/main/java/br/com/ibmec/projetocloud/ikaros/repository/AmigosRepository.java
@@ -1,8 +1,10 @@
 package br.com.ibmec.projetocloud.ikaros.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.ibmec.projetocloud.ikaros.model.Amigos;
 
 public interface AmigosRepository extends JpaRepository<Amigos, Long>{
-    
+    Optional<Amigos> findById(Long Id);
 }
