5c540e1c4b009aa7a8df772d83af116b628f6fe5
Igor Mariano <igor-mariano@outlook.com>
2023-10-04 20:58:54 -0300

    Alterando o generationType de amigos para o ID

--- src/main/java/br/com/ibmec/projetocloud/ikaros/model/Amigos.java
+++ src/main/java/br/com/ibmec/projetocloud/ikaros/model/Amigos.java
@@ -2,6 +2,8 @@ package br.com.ibmec.projetocloud.ikaros.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -14,7 +16,7 @@ import lombok.Data;
 public class Amigos {
 
     @Id
-    @Column(name = "ID_Amizade", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long amizadeId;
 
     @ManyToOne
