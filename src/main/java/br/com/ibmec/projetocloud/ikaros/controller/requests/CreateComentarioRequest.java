37748e209e500d8dc102679882fe0a6818cef7a4
Igor Mariano <igor-mariano@outlook.com>
2023-10-04 21:31:03 -0300

    Adicionando atributos e métodos

--- src/main/java/br/com/ibmec/projetocloud/ikaros/controller/requests/CreateComentarioRequest.java
+++ src/main/java/br/com/ibmec/projetocloud/ikaros/controller/requests/CreateComentarioRequest.java
@@ -2,5 +2,13 @@ package br.com.ibmec.projetocloud.ikaros.controller.requests;
 
 public class CreateComentarioRequest {
 
-    
+    private String conteudo;
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
 }
