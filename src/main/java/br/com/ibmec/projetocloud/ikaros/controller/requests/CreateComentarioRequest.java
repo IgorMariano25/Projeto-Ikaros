1e6e504bb1e2104ab52af721afbd6b5e37e8ff61
Igor Mariano <igor-mariano@outlook.com>
2023-10-05 19:04:36 -0300

    Modificando o CreateComentarioRequest

--- src/main/java/br/com/ibmec/projetocloud/ikaros/controller/requests/CreateComentarioRequest.java
+++ src/main/java/br/com/ibmec/projetocloud/ikaros/controller/requests/CreateComentarioRequest.java
@@ -1,8 +1,41 @@
 package br.com.ibmec.projetocloud.ikaros.controller.requests;
 
+import java.time.LocalDateTime;
+
+import br.com.ibmec.projetocloud.ikaros.model.Postagem;
+import br.com.ibmec.projetocloud.ikaros.model.Usuario;
+
 public class CreateComentarioRequest {
 
+    private Long id;
+    private Usuario comentador;
+    private LocalDateTime dataPublicacaoComentario;
     private String conteudo;
+    private Postagem postagem;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getComentador() {
+        return this.comentador;
+    }
+
+    public Long getIdComentador() {
+        return comentador.getId();
+    }
+
+    public LocalDateTime getDataPublicacaoComentario() {
+        return this.dataPublicacaoComentario;
+    }
+
+    public void setDataPublicacaoComentario(LocalDateTime dataPublicacaoComentario) {
+        this.dataPublicacaoComentario = dataPublicacaoComentario;
+    }
 
     public String getConteudo() {
         return conteudo;
@@ -10,5 +43,17 @@ public class CreateComentarioRequest {
 
     public void setConteudo(String conteudo) {
         this.conteudo = conteudo;
+    }
+
+    public Postagem getPostagem() {
+        return this.postagem;
+    }
+
+    public Long getIdPostagem() {
+        return postagem.getId();
+    }
+
+    public void setPostagem(Postagem postagem) {
+        this.postagem = postagem;
     }
 }
