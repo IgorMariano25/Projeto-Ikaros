db83a26305664033de0e34d06d49416753336da4
Igor Mariano <igor-mariano@outlook.com>
2023-10-06 14:09:51 -0300

    Criando método findAllByUsuarioId

--- src/main/java/br/com/ibmec/projetocloud/ikaros/repository/PostagemRepository.java
+++ src/main/java/br/com/ibmec/projetocloud/ikaros/repository/PostagemRepository.java
@@ -1,10 +1,12 @@
 package br.com.ibmec.projetocloud.ikaros.repository;
 
 import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.ibmec.projetocloud.ikaros.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
     Optional<Postagem> findById(Long Id);
+    List<Postagem> findAllByUsuarioId(Long usuarioId);
 }
