e6f8216ffbed84e325a28cbb1d1998c6a94d736f
Igor Mariano <igor-mariano@outlook.com>
2023-10-09 11:45:38 -0300

    Adicionando atributos e getters/setters

--- src/main/java/br/com/ibmec/projetocloud/ikaros/controller/requests/CreatePostagemRequest.java
+++ src/main/java/br/com/ibmec/projetocloud/ikaros/controller/requests/CreatePostagemRequest.java
@@ -1,5 +1,51 @@
 package br.com.ibmec.projetocloud.ikaros.controller.requests;
 
+import java.time.LocalDateTime;
 public class CreatePostagemRequest {
-    
+
+    private Long postagemId;
+    private String conteudoPost;
+    private String imagem;
+    private Integer curtidas;
+    private LocalDateTime dataHoraPublicacao;
+
+    public Long getPostagemId() {
+        return this.postagemId;
+    }
+
+    public void setPostagemId(Long postagemId) {
+        this.postagemId = postagemId;
+    }
+
+    public String getConteudoPost() {
+        return this.conteudoPost;
+    }
+
+    public void setConteudoPost(String conteudoPost) {
+        this.conteudoPost = conteudoPost;
+    }
+
+    public String getImagem() {
+        return this.imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+
+    public Integer getCurtidas() {
+        return this.curtidas;
+    }
+
+    public void setCurtidas(Integer curtidas) {
+        this.curtidas = curtidas;
+    }
+
+    public LocalDateTime getDataHoraPublicacao() {
+        return this.dataHoraPublicacao;
+    }
+
+    public void setDataHoraPublicacao(LocalDateTime dataHoraPublicacao) {
+        this.dataHoraPublicacao = dataHoraPublicacao;
+    }
 }
