0ede532d539874d8c5801ec0470d99164d687032
Igor Mariano <igor-mariano@outlook.com>
2023-10-09 10:15:24 -0300

    Adicionando atributos com getters/setters

--- src/main/java/br/com/ibmec/projetocloud/ikaros/controller/responses/CreateUsuarioResponse.java
+++ src/main/java/br/com/ibmec/projetocloud/ikaros/controller/responses/CreateUsuarioResponse.java
@@ -1,5 +1,23 @@
 package br.com.ibmec.projetocloud.ikaros.controller.responses;
 
 public class CreateUsuarioResponse {
-    
+
+    private String nome;
+    private String sobrenome;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return this.sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
 }
