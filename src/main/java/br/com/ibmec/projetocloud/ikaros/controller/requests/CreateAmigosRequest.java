9cec020dce3b4dab661da607e820d324bf63af52
Igor Mariano <igor-mariano@outlook.com>
2023-10-09 12:03:39 -0300

    Adicionando atributos e getters/setters

--- src/main/java/br/com/ibmec/projetocloud/ikaros/controller/requests/CreateAmigosRequest.java
+++ src/main/java/br/com/ibmec/projetocloud/ikaros/controller/requests/CreateAmigosRequest.java
@@ -1,5 +1,34 @@
 package br.com.ibmec.projetocloud.ikaros.controller.requests;
 
+import br.com.ibmec.projetocloud.ikaros.model.Usuario;
+
 public class CreateAmigosRequest {
-    
+
+    private Long amizadeId;
+    private Usuario relacionamentoAmizade1;
+    private Usuario relacionamentoAmizade2;
+
+    public Long getAmizadeId() {
+        return this.amizadeId;
+    }
+
+    public void setAmizadeId(Long amizadeId) {
+        this.amizadeId = amizadeId;
+    }
+
+    public Usuario getRelacionamentoAmizade1() {
+        return this.relacionamentoAmizade1;
+    }
+
+    public void setRelacionamentoAmizade1(Usuario relacionamentoAmizade1) {
+        this.relacionamentoAmizade1 = relacionamentoAmizade1;
+    }
+
+    public Usuario getRelacionamentoAmizade2() {
+        return this.relacionamentoAmizade2;
+    }
+
+    public void setRelacionamentoAmizade2(Usuario relacionamentoAmizade2) {
+        this.relacionamentoAmizade2 = relacionamentoAmizade2;
+    }
 }
