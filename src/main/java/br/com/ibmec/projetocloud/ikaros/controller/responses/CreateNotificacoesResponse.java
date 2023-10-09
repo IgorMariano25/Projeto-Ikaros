bb29062268d9bce578d7e62726ece1266f5e6fdf
Igor Mariano <igor-mariano@outlook.com>
2023-10-09 12:07:13 -0300

    Criando atributos e método construtor para CreateNotificacoesResponse

--- src/main/java/br/com/ibmec/projetocloud/ikaros/controller/responses/CreateNotificacoesResponse.java
+++ src/main/java/br/com/ibmec/projetocloud/ikaros/controller/responses/CreateNotificacoesResponse.java
@@ -1,5 +1,21 @@
 package br.com.ibmec.projetocloud.ikaros.controller.responses;
 
+import java.time.LocalDateTime;
+
+import br.com.ibmec.projetocloud.ikaros.model.Usuario;
+
 public class CreateNotificacoesResponse {
-    
+
+    private Boolean visualizado;
+    private LocalDateTime dataHora;
+    private Usuario usuarioOrigem;
+    private Usuario usuarioDestino;
+
+    public CreateNotificacoesResponse(Boolean visualizado, LocalDateTime dataHora, Usuario usuarioOrigem,
+            Usuario usuarioDestino) {
+        this.visualizado = visualizado;
+        this.dataHora = dataHora;
+        this.usuarioOrigem = usuarioOrigem;
+        this.usuarioDestino = usuarioDestino;
+    }
 }
