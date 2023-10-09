58d3a58b60884a597ae79469fbf423f4c510ebae
Igor Mariano <igor-mariano@outlook.com>
2023-10-09 11:54:02 -0300

    Criando método findUsuarioById

--- src/main/java/br/com/ibmec/projetocloud/ikaros/repository/UsuarioRepository.java
+++ src/main/java/br/com/ibmec/projetocloud/ikaros/repository/UsuarioRepository.java
@@ -3,6 +3,8 @@ package br.com.ibmec.projetocloud.ikaros.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.ibmec.projetocloud.ikaros.model.Usuario;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Long countById(Long Id);
+
+    Long findUsuarioById(Long Id);
 }
