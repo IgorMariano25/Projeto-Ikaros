1292fcc1c8d9f5cea82658ca048d728b97aa9c9b
Igor Mariano <igor-mariano@outlook.com>
2023-10-09 11:53:34 -0300

    Adicionando atributos e getters/setters

--- src/main/java/br/com/ibmec/projetocloud/ikaros/controller/responses/CreatePostagemResponse.java
+++ src/main/java/br/com/ibmec/projetocloud/ikaros/controller/responses/CreatePostagemResponse.java
@@ -1,5 +1,73 @@
 package br.com.ibmec.projetocloud.ikaros.controller.responses;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import br.com.ibmec.projetocloud.ikaros.model.Comentario;
+
 public class CreatePostagemResponse {
-    
+
+    private Long postagemId;
+    private Long usuarioId;
+    private String conteudoPost;
+    private String imagem;
+    private Integer curtidas;
+    private LocalDateTime dataHoraPublicacao;
+    private List<Comentario> comentarios;
+
+    public Long getPostagemId() {
+        return this.postagemId;
+    }
+
+    public void setPostagemId(Long postagemId) {
+        this.postagemId = postagemId;
+    }
+
+    public Long getUsuarioId() {
+        return this.usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getConteudoPost() {
+        return this.conteudoPost;
+    }
+
+    public void setConteudoPost(String conteudoPost) {
+        this.conteudoPost = conteudoPost;
+    }
+
+    public String getImagem() {
+        return this.imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+
+    public Integer getCurtidas() {
+        return this.curtidas;
+    }
+
+    public void setCurtidas(Integer curtidas) {
+        this.curtidas = curtidas;
+    }
+
+    public LocalDateTime getDataHoraPublicacao() {
+        return this.dataHoraPublicacao;
+    }
+
+    public void setDataHoraPublicacao(LocalDateTime dataHoraPublicacao) {
+        this.dataHoraPublicacao = dataHoraPublicacao;
+    }
+
+    public List<Comentario> getComentarios() {
+        return this.comentarios;
+    }
+
+    public void setComentarios(List<Comentario> comentarios) {
+        this.comentarios = comentarios;
+    }
 }
