7d6fd04137fde135e2d7503fe4fd9e89b8ce45f2
Igor Mariano <igor-mariano@outlook.com>
2023-10-09 12:05:40 -0300

    Adicionando atributos e getters/setters

--- src/main/java/br/com/ibmec/projetocloud/ikaros/controller/requests/CreateNotificacoesRequest.java
+++ src/main/java/br/com/ibmec/projetocloud/ikaros/controller/requests/CreateNotificacoesRequest.java
@@ -1,5 +1,58 @@
 package br.com.ibmec.projetocloud.ikaros.controller.requests;
 
+import java.time.LocalDateTime;
+
+import br.com.ibmec.projetocloud.ikaros.model.Usuario;
+
 public class CreateNotificacoesRequest {
-    
+
+    private Long notificacoesId;
+    private Boolean visualizado;
+    private LocalDateTime dataHora;
+    private Usuario usuarioOrigem;
+    private Usuario usuarioDestino;
+
+    public Long getNotificacoesId() {
+        return this.notificacoesId;
+    }
+
+    public void setNotificacoesId(Long notificacoesId) {
+        this.notificacoesId = notificacoesId;
+    }
+
+    public Boolean isVisualizado() {
+        return this.visualizado;
+    }
+
+    public Boolean getVisualizado() {
+        return this.visualizado;
+    }
+
+    public void setVisualizado(Boolean visualizado) {
+        this.visualizado = visualizado;
+    }
+
+    public LocalDateTime getDataHora() {
+        return this.dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public Usuario getUsuarioOrigem() {
+        return this.usuarioOrigem;
+    }
+
+    public void setUsuarioOrigem(Usuario usuarioOrigem) {
+        this.usuarioOrigem = usuarioOrigem;
+    }
+
+    public Usuario getUsuarioDestino() {
+        return this.usuarioDestino;
+    }
+
+    public void setUsuarioDestino(Usuario usuarioDestino) {
+        this.usuarioDestino = usuarioDestino;
+    }
 }
