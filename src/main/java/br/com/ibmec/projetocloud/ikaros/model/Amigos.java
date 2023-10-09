d50039a522a9c2c20f0751304789e79293e0d8cd
Igor Mariano <igor-mariano@outlook.com>
2023-10-09 12:01:38 -0300

    Removendo import desnecessário

--- src/main/java/br/com/ibmec/projetocloud/ikaros/model/Amigos.java
+++ src/main/java/br/com/ibmec/projetocloud/ikaros/model/Amigos.java
@@ -1,6 +1,5 @@
 package br.com.ibmec.projetocloud.ikaros.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
