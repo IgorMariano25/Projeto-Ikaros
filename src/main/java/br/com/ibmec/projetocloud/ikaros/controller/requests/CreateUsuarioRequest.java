883a9f62a71a5c9165d1135a2e09b8838bda4ba8
Igor Mariano <igor-mariano@outlook.com>
2023-10-09 10:01:34 -0300

    Adicioando atributos e getters/setters

--- src/main/java/br/com/ibmec/projetocloud/ikaros/controller/requests/CreateUsuarioRequest.java
+++ src/main/java/br/com/ibmec/projetocloud/ikaros/controller/requests/CreateUsuarioRequest.java
@@ -1,5 +1,32 @@
 package br.com.ibmec.projetocloud.ikaros.controller.requests;
 
 public class CreateUsuarioRequest {
-    
+
+    private Long usuarioId;
+    private String nome;
+    private String sobrenome;
+
+    public Long getUsuarioId() {
+        return this.usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return this.sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
 }
